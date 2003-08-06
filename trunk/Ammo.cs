3b3f5420ceff3ac3c00206ba95ca3b6607267c00
Adam Milazzo <adam@adammil.net>
2003-08-06 11:40:58 +0000

    Tons of stuff... started getting server/client stuff working, but much needs to be done.

--- trunk/Ammo.cs
+++ trunk/Ammo.cs
@@ -2,37 +2,73 @@ using System;
 using System.Drawing;
 using GameLib.Mathematics;
 using GameLib.Video;
+using GameLib.IO;
 
 namespace TriangleChase
 {
 
-public interface Ammo
-{ Ship Owner { get; }
+public abstract class PointAmmo : PointObject
+{ public PointAmmo() { }
+  public PointAmmo(Ship owner, VectorF pos, VectorF vel) { this.owner=owner; Pos=pos; Vel=vel; }
+
+  public override int SizeOf() { return base.SizeOf()+4; }
+  public override void SerializeTo(byte[] buf, int index)
+  { IOH.WriteLE4(buf, index, (int)owner.ID); index += 4;
+    base.SerializeTo(buf, index);
+  }
+  public override void DeserializeFrom(byte[] buf, int index)
+  { owner = world.FindShip(IOH.ReadLE4U(buf, index)); index += 4;
+    base.DeserializeFrom(buf, index);
+  }
+  
+  protected Ship owner;
 }
 
-public abstract class PointAmmo : PointObject, Ammo
-{ public PointAmmo(Ship owner, VectorF pos, VectorF vel) { this.owner=owner; Pos=pos; Vel=vel; }
-  public Ship Owner { get { return owner; } }
-  protected Ship owner; // TODO: move this to object level?
-}
+public abstract class SphereAmmo : SphereObject
+{ public SphereAmmo(int radius) { Radius=radius; }
+  public SphereAmmo(Ship owner, VectorF pos, VectorF vel, int radius)
+  { this.owner=owner; Pos=pos; Vel=vel; Radius=radius;
+  }
 
-public abstract class SphereAmmo : SphereObject, Ammo
-{ public SphereAmmo(Ship owner, VectorF pos, VectorF vel, int radius) { this.owner=owner; Pos=pos; Vel=vel; Radius=radius; }
-  public Ship Owner { get { return owner; } }
-  protected Ship owner; // TODO: move this to object level?
+  public override int SizeOf() { return base.SizeOf()+4; }
+  public override void SerializeTo(byte[] buf, int index)
+  { IOH.WriteLE4(buf, index, (int)owner.ID); index += 4;
+    base.SerializeTo(buf, index);
+  }
+  public override void DeserializeFrom(byte[] buf, int index)
+  { owner = world.FindShip(IOH.ReadLE4U(buf, index)); index += 4;
+    base.DeserializeFrom(buf, index);
+  }
+  
+  protected Ship owner;
 }
 
 public class Bullet : PointAmmo
-{ public Bullet(Ship owner, VectorF pos, VectorF vel) : base(owner, pos, vel) { Color=Color.White; Weight=1; }
+{ public Bullet() { Color=Color.White; Weight=1; }
+  public Bullet(Ship owner, VectorF pos, VectorF vel) : base(owner, pos, vel) { Color=Color.White; Weight=1; }
   public Bullet(Ship owner, VectorF pos, VectorF vel, Color color) : base(owner, pos, vel) { Color=color; Weight=1; }
+
   public override float CalcDamage(Ship ship, VectorF vec) { return base.CalcDamage(ship, vec)+2.5f; }
   public override void Draw(Surface dest, int cx, int cy) { dest.PutPixel(RX-cx, RY-cy, Color); }
+
+  public override int SizeOf() { return base.SizeOf()+3; }
+  public override void SerializeTo(byte[] buf, int index)
+  { buf[index++] = Color.R;
+    buf[index++] = Color.G;
+    buf[index++] = Color.B;
+    base.SerializeTo(buf, index);
+  }
+  public override void DeserializeFrom(byte[] buf, int index)
+  { Color = Color.FromArgb(buf[index], buf[index+1], buf[index+2]); index += 3;
+    base.DeserializeFrom(buf, index);
+  }
 
   public Color Color;
 }
 
 public class SpriteAmmo : SphereAmmo
-{ public SpriteAmmo(Ship owner, VectorF pos, VectorF vel, int radius, Sprite sprite) : base(owner, pos, vel, radius)
+{ public SpriteAmmo(int radius, Sprite sprite) : base(radius) { this.sprite=sprite; }
+  public SpriteAmmo(Ship owner, VectorF pos, VectorF vel, int radius, Sprite sprite) : base(owner, pos, vel, radius)
   { this.sprite=sprite;
   }
   public override void Draw(Surface dest, int cx, int cy) { CenterBlit(dest, Globals.Sprite(sprite), cx, cy); }
@@ -40,7 +76,11 @@ public class SpriteAmmo : SphereAmmo
 }
 
 public class CannonBall : SpriteAmmo
-{ public CannonBall(Ship owner, VectorF pos, VectorF vel) : base(owner, pos, vel, 3, Sprite.CannonBall) { Weight=25; }
+{ public CannonBall() : base(3, Sprite.CannonBall) { Weight=25; NetPolicy = NetPolicy.RemoteAll; }
+  public CannonBall(Ship owner, VectorF pos, VectorF vel) : base(owner, pos, vel, 3, Sprite.CannonBall)
+  { Weight=25;
+    NetPolicy = NetPolicy.RemoteAll;
+  }
   public override float CalcDamage(Ship ship, VectorF vec) { return base.CalcDamage(ship, vec)+10; }
   public override void HitMap()
   { world.Explode(owner, Explosion.Tiny, Pos);
@@ -53,7 +93,8 @@ public class CannonBall : SpriteAmmo
 }
 
 public class Grenade : SpriteAmmo
-{ public Grenade(Ship owner, VectorF pos, VectorF vel) : base(owner, pos, vel, 2, Sprite.Grenade)
+{ public Grenade() : base(2, Sprite.Grenade) { Weight=5; RadiusSqr=8; }
+  public Grenade(Ship owner, VectorF pos, VectorF vel) : base(owner, pos, vel, 2, Sprite.Grenade)
   { Weight=5;
     RadiusSqr=8;
   }
@@ -65,14 +106,15 @@ public class Grenade : SpriteAmmo
   public override void Think()
   { if(Age>world.TPS/2)
     { Remove=true;
-      for(int a=0; a<256; a+=Globals.Random.Next(8))
-      { VectorF v = Globals.Vector(a);
-        for(int n=0; n<2; n++)
-        { int cn = Globals.Random.Next(3); if(cn>2) cn=2;
-          world.AddObject(new Bullet(owner, Pos+v*((float)Globals.Random.NextDouble()*4),
-                                     Vel+v*((float)Globals.Random.NextDouble()/2), owner.ColorMap[cn]));
+      if(world.IsServer)
+        for(int a=0; a<256; a+=Globals.Random.Next(8))
+        { VectorF v = Globals.Vector(a);
+          for(int n=0; n<2; n++)
+          { int cn = Globals.Random.Next(3); if(cn>2) cn=2;
+            App.Server.AddObject(new Bullet(owner, Pos+v*((float)Globals.Random.NextDouble()*4),
+                                            Vel+v*((float)Globals.Random.NextDouble()/2), owner.ColorMap[cn]));
+          }
         }
-      }
     }
   }
 }
